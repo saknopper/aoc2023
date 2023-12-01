b190c84537140a677e9e9e3fc3b3c3b6894a2eed
Sander Knopper <s.knopper@flusso.nl>
2023-12-01 11:53:09 +0100

    Day 1a

--- src/main/java/com/github/saknopper/aoc2023/days/Day01.java
+++ src/main/java/com/github/saknopper/aoc2023/days/Day01.java
@@ -1,5 +1,6 @@
 package com.github.saknopper.aoc2023.days;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -7,8 +8,13 @@ public class Day01 extends Day {
     @Override
     public String getAnswerPartOne() throws Exception {
         Path path = Paths.get(getClass().getClassLoader().getResource("day01.txt").toURI());
+        var sum = Files.lines(path)
+                .map(line -> line.chars().mapToObj(c -> (char) c).filter(Character::isDigit).toList())
+                .map(digitList -> digitList.getFirst().toString() + digitList.getLast().toString())
+                .mapToInt(Integer::valueOf)
+                .sum();
 
-        return "TODO";
+        return String.valueOf(sum);
     }
 
     @Override
