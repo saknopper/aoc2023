310f0d232c31893ae704e36f8f33c302f23b8554
Sander Knopper <s.knopper@flusso.nl>
2023-12-01 14:00:17 +0100

    Day 1b

--- src/main/java/com/github/saknopper/aoc2023/days/Day01.java
+++ src/main/java/com/github/saknopper/aoc2023/days/Day01.java
@@ -3,8 +3,22 @@ package com.github.saknopper.aoc2023.days;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class Day01 extends Day {
+    private static final Map<String, String> SPELLED_OUT_MAP =
+        Map.of(
+            "one", "1",
+            "two", "2",
+            "three", "3",
+            "four", "4",
+            "five", "5",
+            "six", "6",
+            "seven", "7",
+            "eight", "8",
+            "nine", "9"
+        );
+
     @Override
     public String getAnswerPartOne() throws Exception {
         Path path = Paths.get(getClass().getClassLoader().getResource("day01.txt").toURI());
@@ -19,6 +33,29 @@ public class Day01 extends Day {
 
     @Override
     public String getAnswerPartTwo() throws Exception {
-        return "TODO";
+        Path path = Paths.get(getClass().getClassLoader().getResource("day01.txt").toURI());
+        var sum = Files.lines(path)
+                .map(Day01::replaceSpelledOutNumbers)
+                .map(line -> line.chars().mapToObj(c -> (char) c).filter(Character::isDigit).toList())
+                .map(digitList -> digitList.getFirst().toString() + digitList.getLast().toString())
+                .mapToLong(Long::valueOf)
+                .sum();
+
+        return String.valueOf(sum);
+    }
+
+    private static String replaceSpelledOutNumbers(String input) {
+        var result = new StringBuilder(input);
+
+        for (int i = 0; i < result.length(); i++) {
+            for (var entry : SPELLED_OUT_MAP.entrySet()) {
+                var end = Math.min(i + entry.getKey().length(), result.length());
+                if (entry.getKey().contentEquals(result.subSequence(i, end))) {
+                    result.setCharAt(i, entry.getValue().charAt(0));
+                }
+            }
+        }
+
+        return result.toString();
     }
 }
